049e9b1f1945862c4ed5a80d3375c9546e6793d5
Pivotingmum <joladeosuntubo@gmail.com>
2022-03-16 18:18:28 -0600

    created scenarios

---
+++ js/app.js
@@ -0,0 +1,11 @@
+console.log('Hello world');
+
+var userAge = 18;
+var is_subscribed = true;
+
+
+
+if(userAge < 18 && (!is_subscribed)){console.log("the user is younger than 18 and not subscribed");
+}else if(userAge >= 18 && (!is_subscribed)){console.log("the user is  18 or older and not subscribed");
+}else if (userAge < 18 && is_subscribed){console.log("the user is younger than 18 and is subscribed");
+}else if(userAge >= 18 && is_subscribed){console.log("the user is 18 or older and is subscribed");}
