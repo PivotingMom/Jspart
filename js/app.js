74fd8cbf60d1ae013d1ff691f2791ba8c585f3b9
Pivotingmum <joladeosuntubo@gmail.com>
2022-03-18 23:02:23 -0600

    wk9c

--- js/app.js
+++ js/app.js
@@ -1,12 +1,28 @@
-console.log('Hello world');
+// console.log('Hello world');
 
- var userAge = 18;
- var is_subscribed = true;
+//  var userAge = 18;
+//  var is_subscribed = true;
 
 
 
-if(userAge < 18 && (!is_subscribed)){console.log("the user is younger than 18 and not subscribed"); }else if(userAge >= 18 && (!is_subscribed)){console.log("the user is  18 or older and not subscribed");
-}else if (userAge < 18 && is_subscribed){console.log("the user is younger than 18 and is subscribed");
-}else if(userAge >= 18 && is_subscribed){console.log("the user is 18 or older and is subscribed");} 
+// if(userAge < 18 && (!is_subscribed)){console.log("the user is younger than 18 and not subscribed"); }else if(userAge >= 18 && (!is_subscribed)){console.log("the user is  18 or older and not subscribed");
+// }else if (userAge < 18 && is_subscribed){console.log("the user is younger than 18 and is subscribed");
+// }else if(userAge >= 18 && is_subscribed){console.log("the user is 18 or older and is subscribed");} 
+
+var tweets = [
+  "TGIF","Spring-2022", "Spring break begins", "friyay", "family weekend", "spring-sale", "end of season sale", "pray for Ukraine", "hackathon loading", "Break the Bias"];
+
+  var userName = [
+    "@jola", "@tina", "@dipo", "@paris", "@george", "@dan", "@yinka", "@esther", "@liz", "@israel"];
+
+  var doc = [
+    "01-03-2022", "02-03-2022", "03-03-2022", "04-03-2022", "05-03-2022", "06-03-2022", "07-03-2022", "08-03-2022", "09-03-2022", "10-03-2022"];
+    
+  var userAge = [
+    "16", "16", "16", "16", "16", "16", "16", "16", "16", "16",]
+
+//objects are collection of properties 
+console.log(tweets);
+
 
 
